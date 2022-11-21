f6e963c5d244a1b4054f9d3de6df18d721fb0d19
Bruno Larraín <bruno.larrain.e@gmail.com>
2022-11-21 20:10:58 -0300

    feat: agregadr sección professionals

---
+++ components/CollapsibleCard.js
@@ -0,0 +1,86 @@
+const collapsibleCards = document.querySelectorAll(".profile");
+console.log(collapsibleCards);
+
+collapsibleCards.forEach((card) =>
+  card.addEventListener("click", () => expand(card))
+);
+
+function expand(card) {
+  card.classList.toggle("profile--expanded");
+
+  // If card is not expanded after toggle, add 'unexpanded' class
+  if (!card.classList.contains("profile--expanded"))
+    card.classList.toggle("profile--unexpanded");
+  // Else if card is expanded after toggle and still contains 'unexpanded' class, remove 'unexpanded'
+  else if (
+    card.classList.contains("profile--expanded") &&
+    card.classList.contains("profile--unexpanded")
+  )
+    card.classList.toggle("profile--unexpanded");
+}
+
+function toggleTheme() {
+  let docu = document.querySelector("html");
+
+  docu.classList.toggle("light-theme");
+  docu.classList.toggle("dark-theme");
+}
+
+// export default CollapsibleCard {
+//     constructor({ cardName, src }, templateSelector, { handleCardClick }) {
+//         this._name = cardName;
+//         this._src = src;
+//         this._template = templateSelector;
+//         this._handleCardClick = handleCardClick;
+//       }
+
+//       _getTemplate() {
+//         const cardElement = this._template.cloneNode(true);
+//         return cardElement;
+//       }
+
+//       generateCard({ cardName, cardImage, cardLike, cardDelete }) {
+//         this._element = this._getTemplate();
+//         this._cardName = this._element.querySelector(cardName);
+//         this._cardImage = this._element.querySelector(cardImage);
+//         this._cardLike = this._element.querySelector(cardLike);
+//         this._cardDelete = this._element.querySelector(cardDelete);
+//         this._setEventListeners();
+//         this._cardName.textContent = this._name;
+//         this._cardImage.src = this._src;
+//         this._cardImage.alt = `Fotografía subida de ${this._name}`;
+//         return this._element;
+//       }
+
+//       _setEventListeners() {
+//         this._cardImage.addEventListener("click", this._handleCardClick);
+//         this._cardDelete.addEventListener("click", () => this._element.remove());
+//         this._cardLike.addEventListener("click", this._like);
+//       }
+
+//       _like(evt) {
+//         evt.target.classList.toggle("elements__like-btn_active");
+//       }
+
+//       _expand(card) {
+//             card.classList.toggle("profile--expanded");
+
+//             // If card is not expanded after toggle, add 'unexpanded' class
+//             if (!card.classList.contains("profile--expanded"))
+//               card.classList.toggle("profile--unexpanded");
+//             // Else if card is expanded after toggle and still contains 'unexpanded' class, remove 'unexpanded'
+//             else if (
+//               card.classList.contains("profile--expanded") &&
+//               card.classList.contains("profile--unexpanded")
+//             )
+//               card.classList.toggle("profile--unexpanded");
+//           }
+
+//           _toggleTheme() {
+//             let docu = document.querySelector("html");
+
+//             docu.classList.toggle("light-theme");
+//             docu.classList.toggle("dark-theme");
+
+//           }
+//       }
