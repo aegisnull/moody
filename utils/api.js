29d595585afdb7bed92da2ab38f987e266daaffd
Luis Tellez <luis@luistellez.com>
2022-11-23 11:22:29 -0800

    feat: add api.js

---
+++ utils/api.js
@@ -0,0 +1,34 @@
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error ${res.status}`);
+  }
+
+  //GET https://data.mongodb-api.com/app/data-osktx/endpoint/stories
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/stories`, {
+      method: "GET",
+      headers: this._headers,
+    })
+      .then(this._checkResponse)
+      .then((data) => {
+        return console.log(data);
+      });
+  }
+}
+
+const api = new Api({
+  baseUrl: "https://data.mongodb-api.com/app/data-osktx/endpoint",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
